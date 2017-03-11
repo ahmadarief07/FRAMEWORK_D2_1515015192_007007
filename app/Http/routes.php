75a55c6a7b00fa16c5064f12bb95fe207916f97a
Ahmad Arifin <ariefahh07@gmail.com>
2017-03-11 12:20:28 +0800

    posttest2

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route :: get('Hello-World', function() {
+	return 'Hello World';
+	});
+
+Route::get("pengguna/{pengguna}", function($pengguna)
+	{
+	return "Hello World dari pengguna $pengguna";
+	});
+
+Route::get('kelasd2/framework/{mhs?}', function ($mhs="Anonim")
+	{
+		return "Selamat Datang $mhs";
+	});
